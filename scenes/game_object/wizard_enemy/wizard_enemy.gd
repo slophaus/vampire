71e6d3505a2b04879f28566866a77c64ed2616e7
slophaus <brandon.a.lester@gmail.com>
2025-12-27 14:32:03 -0700

    Tune speeds, spawns, and wizard spells

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,11 +1,19 @@
 extends CharacterBody2D
 
+@export var spell_ability: PackedScene
+@export var spell_range := 450.0
+@export var spell_cooldown := 1.5
+@export var spell_damage := 6.0
+
 @onready var visuals := $Visuals
 @onready var velocity_component: VelocityComponent = $VelocityComponent
+@onready var spell_timer: Timer = $SpellTimer
 
 
 func _ready():
 	$HurtboxComponent.hit.connect(on_hit)
+	spell_timer.wait_time = spell_cooldown
+	spell_timer.timeout.connect(on_spell_timer_timeout)
 
 
 func _process(delta):
@@ -19,3 +27,18 @@ func _process(delta):
 
 func on_hit():
 	$HitRandomAudioPlayerComponent.play_random()
+
+
+func on_spell_timer_timeout():
+	if spell_ability == null:
+		return
+
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+
+	var spell_instance = spell_ability.instantiate() as SpellAbility
+	var foreground_layer = get_tree().get_first_node_in_group("foreground_layer")
+	foreground_layer.add_child(spell_instance)
+	spell_instance.hitbox_component.damage = spell_damage
+	spell_instance.setup(global_position, player.global_position, spell_range)
