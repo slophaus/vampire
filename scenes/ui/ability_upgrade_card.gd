51c96461f8a7b7e56f6fee959dec0f27e1cdccf1
slophaus <brandon.a.lester@gmail.com>
2025-12-27 14:05:50 -0700

    Revert "Highlight focused upgrade cards with yellow border and remove hover rotation to prevent jiggle"

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -7,18 +7,13 @@ signal selected
 @onready var description_label: Label = %DescriptionLabel
 
 var disabled := false
-var normal_style: StyleBox
-var highlighted_style: StyleBox
 
 
 func _ready():
 	gui_input.connect(on_gui_input)
 	mouse_entered.connect(on_mouse_entered)
-	mouse_exited.connect(on_mouse_exited)
 	focus_entered.connect(on_focus_entered)
-	focus_exited.connect(on_focus_exited)
 	focus_mode = Control.FOCUS_ALL
-	_setup_styles()
 
 
 func play_in(delay: float = 0):
@@ -38,7 +33,6 @@ func set_ability_upgrade(upgrade: AbilityUpgrade) -> void:
 
 func select_card():
 	disabled = true
-	_clear_highlight()
 	$AnimationPlayer.play("selected")
 	
 	# make other cards disappear
@@ -72,48 +66,3 @@ func on_focus_entered():
 		return
 
 	$HoverAnimationPlayer.play("hover")
-	_apply_highlight()
-
-
-func on_mouse_exited():
-	if disabled:
-		return
-
-	if not has_focus():
-		_clear_highlight()
-
-
-func on_focus_exited():
-	if disabled:
-		return
-
-	_clear_highlight()
-
-
-func _setup_styles():
-	var theme_style = get_theme_stylebox("panel")
-	if theme_style:
-		normal_style = theme_style.duplicate()
-	else:
-		normal_style = StyleBoxFlat.new()
-
-	highlighted_style = normal_style.duplicate()
-	if highlighted_style is StyleBoxFlat:
-		var style = highlighted_style as StyleBoxFlat
-		style.border_width_left = 4
-		style.border_width_top = 4
-		style.border_width_right = 4
-		style.border_width_bottom = 4
-		style.border_color = Color(1.0, 0.9, 0.25)
-
-	add_theme_stylebox_override("panel", normal_style)
-
-
-func _apply_highlight():
-	if highlighted_style:
-		add_theme_stylebox_override("panel", highlighted_style)
-
-
-func _clear_highlight():
-	if normal_style:
-		add_theme_stylebox_override("panel", normal_style)
