e4eee9b5fee2c37cd7c4ed040b5fc48ca5a0fb82
slophaus <brandon.a.lester@gmail.com>
2025-12-29 21:38:18 -0700

    Destroy swords on worm collision

--- scenes/ability/sword_ability/sword_ability.gd
+++ scenes/ability/sword_ability/sword_ability.gd
@@ -3,6 +3,7 @@ class_name SwordAbility
 
 const SPEED := 450.0
 const MAX_HITS := 3
+const WORM_COLLISION_LAYER := 1
 
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -19,7 +20,9 @@ func _ready():
 	animation_player.stop()
 	sprite.scale = Vector2.ONE
 	collision_shape.disabled = false
+	hitbox_component.collision_mask = WORM_COLLISION_LAYER
 	hitbox_component.hit_landed.connect(on_hit_landed)
+	hitbox_component.body_entered.connect(on_body_entered)
 
 
 func _physics_process(delta: float) -> void:
@@ -45,4 +48,9 @@ func setup(start_position: Vector2, target_position: Vector2, range_limit: float
 func on_hit_landed(current_hits: int) -> void:
 	hit_count = current_hits
 	if hit_count >= MAX_HITS:
+		queue_free()
+
+
+func on_body_entered(body: Node) -> void:
+	if body != null and body.is_in_group("worm"):
 		queue_free()
