409c46ca1874df5e88b4719223b13aa598461a7e
brandon <brandon.a.lester@gmail.com>
2025-12-30 22:43:09 -0700

    WIP

--- scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -6,7 +6,7 @@ var base_damage = 10
 var additional_damage_percent: float = 1.0
 var base_penetration := 3
 var axe_level := 1
-var multi_shot_delay := 0.1
+var multi_shot_delay := 0.3
 var player_number := 1
 
 
