cbff0a0a297f262077f19d2f2985547e02d82268
slophaus <brandon.a.lester@gmail.com>
2025-12-31 16:56:37 -0700

    Update dragon boss abilities and minion spawns

--- scenes/game_object/boss_enemy/boss_enemy.gd
+++ scenes/game_object/boss_enemy/boss_enemy.gd
@@ -13,7 +13,7 @@ const ENEMY_TYPES = {
 		"max_speed": 70,
 		"acceleration": 2.5,
 		"facing_multiplier": 1,
-		"contact_damage": 4
+		"contact_damage": 5
 	},
 	2: {
 		"max_health": 150.0,
@@ -27,15 +27,22 @@ const ENEMY_TYPES = {
 const SEPARATION_RADIUS := 15.0
 const SEPARATION_PUSH_STRENGTH := 5.0
 const MOUSE_DIG_LEVEL_TWO_TINT := Color(0.25, 0.25, 1)
+const MINION_SPAWN_COUNT := 4
+const MINION_SPAWN_RADIUS := 32.0
 @export var enemy_index := 1
+@export var minion_scene: PackedScene = preload("res://scenes/game_object/basic_enemy/basic_enemy.tscn")
+@export var minion_spawn_interval_range := Vector2(6.0, 10.0)
 
 @onready var visuals := $Visuals
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var health_bar: ProgressBar = $HealthBar
 @onready var hit_flash_component = $HitFlashComponent
 @onready var death_component = $DeathComponent
 @onready var fireball_ability_controller = $Abilities/FireballAbilityController
 @onready var dig_ability_controller = $Abilities/DigAbilityController
+@onready var sword_ability_controller = $Abilities/SwordAbilityController
+@onready var minion_spawn_timer: Timer = $MinionSpawnTimer
 @onready var mouse_sprite: AnimatedSprite2D = $Visuals/mouse_sprite
 @onready var dragon_sprite: AnimatedSprite2D = $Visuals/dragon_sprite
 @onready var rat_sprite: Sprite2D = $Visuals/RatSprite
@@ -52,8 +59,13 @@ func _ready():
 	$HurtboxComponent.hit.connect(on_hit)
 	apply_enemy_type(enemy_index)
 	fireball_ability_controller.fireball_level = 3
+	sword_ability_controller.sword_level = 2
 	assign_mouse_dig_level()
 	apply_enemy_tint_for_type()
+	health_component.health_changed.connect(update_health_display)
+	update_health_display()
+	minion_spawn_timer.timeout.connect(on_minion_spawn_timer_timeout)
+	schedule_next_minion_spawn()
 
 
 func _physics_process(delta):
@@ -104,6 +116,11 @@ func apply_enemy_type(index: int) -> void:
 	rat_sprite.visible = enemy_index == 2
 	fireball_ability_controller.set_active(enemy_index == 1)
 	dig_ability_controller.set_active(enemy_index == 0)
+	sword_ability_controller.set_active(enemy_index == 1)
+	if enemy_index == 1:
+		schedule_next_minion_spawn()
+	else:
+		minion_spawn_timer.stop()
 
 	rat_sprite.texture = rat_texture
 
@@ -152,3 +169,45 @@ func assign_mouse_dig_level() -> void:
 	rng.randomize()
 	mouse_has_dig_level_two = rng.randf() < 0.1
 	dig_ability_controller.set_dig_level(2 if mouse_has_dig_level_two else 1)
+
+
+func update_health_display() -> void:
+	if health_bar == null:
+		return
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_minion_spawn_timer_timeout() -> void:
+	if enemy_index != 1:
+		return
+	spawn_minions()
+	schedule_next_minion_spawn()
+
+
+func schedule_next_minion_spawn() -> void:
+	if minion_spawn_timer == null or enemy_index != 1:
+		return
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	var min_interval = minion_spawn_interval_range.x
+	var max_interval = max(minion_spawn_interval_range.x, minion_spawn_interval_range.y)
+	minion_spawn_timer.wait_time = rng.randf_range(min_interval, max_interval)
+	minion_spawn_timer.start()
+
+
+func spawn_minions() -> void:
+	if minion_scene == null:
+		return
+	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
+	var spawn_parent: Node = entities_layer if entities_layer != null else get_parent()
+	if spawn_parent == null:
+		return
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	for i in range(MINION_SPAWN_COUNT):
+		var minion = minion_scene.instantiate() as Node2D
+		minion.set("enemy_index", 1)
+		spawn_parent.add_child(minion)
+		var angle = rng.randf_range(0.0, TAU)
+		var offset = Vector2(cos(angle), sin(angle)) * MINION_SPAWN_RADIUS
+		minion.global_position = global_position + offset
