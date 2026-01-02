82b636c28d9dac3313c91317fb1d8fc961412b0d
slophaus <brandon.a.lester@gmail.com>
2026-01-01 18:50:13 -0700

    Reduce blood droplet size and fade out

--- scenes/game_object/blood_droplet/blood_droplet.gd
+++ scenes/game_object/blood_droplet/blood_droplet.gd
@@ -1,15 +1,17 @@
 extends Node2D
 class_name BloodDroplet
 
-@export var radius: float = 4.0
+@export var radius: float = 2.5
 @export var color: Color = Color(0.75, 0.05, 0.05)
-@export var dry_up_seconds: float = 0.0
+@export var dry_up_seconds: float = 1.5
 
 
 func _ready() -> void:
 	queue_redraw()
 	if dry_up_seconds > 0.0:
-		get_tree().create_timer(dry_up_seconds).timeout.connect(queue_free)
+		var tween = create_tween()
+		tween.tween_property(self, "modulate:a", 0.0, dry_up_seconds).from(1.0)
+		tween.tween_callback(queue_free)
 
 
 func _draw() -> void:
