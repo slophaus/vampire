e1e8ee7b294c068bec39a1a1d95941f36c4ebdfc
slophaus <brandon.a.lester@gmail.com>
2026-01-05 04:29:22 -0700

    Move enemy stats to exported fields

--- resources/enemy_stats.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name EnemyStats
-
-@export var max_health: float = 10.0
-@export var max_speed: float = 30.0
-@export var acceleration: float = 5.0
-@export var facing_multiplier: float = -1.0
-@export var contact_damage: float = 1.0
-@export var poison_contact_duration: float = 0.0
