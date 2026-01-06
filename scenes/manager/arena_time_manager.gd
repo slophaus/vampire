6c216ffdd3b5993831ca0ad827c8e5e81859ead4
slophaus <brandon.a.lester@gmail.com>
2026-01-05 18:01:04 -0700

    Add per-level arena timer state

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -10,14 +10,18 @@ const DIFFICULTY_INTERVAL := 15
 @onready var timer = $Timer
 
 var arena_difficulty = 1
+var base_duration := 0.0
+var total_duration := 0.0
 
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+	base_duration = timer.wait_time
+	total_duration = base_duration
 
 
 func _process(delta):
-	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	var next_time_target = total_duration - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
 	if timer.time_left <= next_time_target:
 		arena_difficulty += 1
 		arena_difficulty_increased.emit(arena_difficulty)
@@ -25,7 +29,7 @@ func _process(delta):
 
 
 func get_time_elapsed():
-	return timer.wait_time - timer.time_left
+	return total_duration - timer.time_left
 
 
 func get_arena_difficulty() -> int:
@@ -33,8 +37,31 @@ func get_arena_difficulty() -> int:
 
 
 func get_time_until_next_difficulty() -> float:
-	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	var next_time_target = total_duration - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
 	return max(0.0, timer.time_left - next_time_target)
+
+
+func get_state() -> Dictionary:
+	return {
+		"arena_difficulty": arena_difficulty,
+		"time_left": timer.time_left,
+		"total_duration": total_duration,
+	}
+
+
+func apply_state(state: Dictionary) -> void:
+	arena_difficulty = int(state.get("arena_difficulty", 1))
+	total_duration = float(state.get("total_duration", base_duration))
+	var time_left = float(state.get("time_left", total_duration))
+	timer.stop()
+	timer.start(max(time_left, 0.0))
+
+
+func reset_state() -> void:
+	arena_difficulty = 1
+	total_duration = base_duration
+	timer.stop()
+	timer.start(base_duration)
 
 
 func on_timer_timeout():
